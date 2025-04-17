05f7a860f3c47491270fee9005deff51cd5a74d2
Corbanator <corbandioxide42@gmail.com>
2025-04-16 21:18:15 -0600

    Break main functionality into separate file

--- wavefunction_collapse.py
+++ wavefunction_collapse.py
@@ -1,52 +1,6 @@
 import random
-from player import Player  # Import the Player class
-from pynput import keyboard  # Import keyboard listener
-import colorama
 from position import Position, Direction
-from tileset import Tileset
 from map import Map
-from mapVisual import MapVisualizer
-
-
-def main():
-    tiles = {"L", "B", "O"}
-    tileset = Tileset(tiles,
-                      colors={
-                              "O": colorama.Fore.BLUE,
-                              "B": colorama.Fore.YELLOW,
-                              "L": colorama.Fore.GREEN,
-                              })
-    tileset.add_rule({"L"}, Direction.all_cardinal(), {"L", "B"})
-    tileset.add_rule({"B", "O"}, Direction.all_cardinal(), {"B", "O"})
-    dimensions = (10,10)
-    map = Map(dimensions, tileset)
-    map_generation(map)
-    player = Player(dimensions)  # Initialize player with randomized position
-    while map.get_tile(player.pos) == "O":
-        player = Player(dimensions)  # Reinitialize player until not on 'O'
-    player.update_map(map)  # Place player on the map
-
-    print(
-            "\nWelcome to the WFC World Generator Game!\n"
-            "Navigate the player (Orange) using the arrow keys.\n"
-            "Press buttons (Purple) to unlock the goal(Red -> Dark Green).\n"
-            "Avoid obstacles like the ocean (Blue).\n"
-            "Explore the land (Green) and beach (Tan).\n"
-            "Good luck!\n"
-        )
-
-    visualizer = MapVisualizer(map, player)  # Initialize the visualizer
-
-    def on_keypress(event):
-        player.handle_keypress(event, map, visualizer)  # Delegate keypress handling to Player
-
-    visualizer.root.bind("<Up>", lambda event: on_keypress(event))
-    visualizer.root.bind("<Left>", lambda event: on_keypress(event))
-    visualizer.root.bind("<Down>", lambda event: on_keypress(event))
-    visualizer.root.bind("<Right>", lambda event: on_keypress(event))
-    visualizer.root.bind("q", lambda event: on_keypress(event))  # Bind 'q' key to quit
-
-    visualizer.run()  # Run the tkinter visualization
 
 
 def map_generation(map: Map):
@@ -59,6 +13,7 @@ def map_generation(map: Map):
                 map.set_tile(Position(i, j), choice)
                 propagate_collapse(map, Position(i, j), None)
     return map
+
 
 def propagate_collapse(map: Map, position: Position, direction: Direction | None = None):
     prop_source = map.get_tile(position)
@@ -74,6 +29,3 @@ def propagate_collapse(map: Map, position: Position, direction: Direction | None
             map.set_tile(target, new_options)
             if new_options != target_options:
                 propagate_collapse(map, target, dir.opposite())
-
-if __name__ == "__main__":
-    main()
