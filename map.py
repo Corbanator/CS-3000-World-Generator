6fabb5a33f0861cbf1d3fa6bb57d715b6f613b5d
Corbanator <corbandioxide42@gmail.com>
2025-04-21 16:42:00 -0600

    Add actual multithreading!!! It's Alive!

--- map.py
+++ map.py
@@ -3,37 +3,65 @@ from position import Position, Direction
 from tileset import Tileset
 
 class Map:
-    def __init__(self, dimensions: tuple[int, int], tileset: Tileset):
+    def __init__(self, dimensions: tuple[int, int], tileset: Tileset, looping: bool = False):
         self.dimensions = dimensions
         num_tiles = dimensions[0] * self.dimensions[1]
         self.visual_tiles: list[str] = ["e" for _ in range(num_tiles)]
         self.original_tiles: list[set[str] | str] = [tileset.tiles for _ in range(num_tiles)]   # Store original tiles
         self.tileset = tileset
+        self.looping = looping
 
     def get_tile(self, pos: Position):
-        return self.original_tiles[pos.y * self.dimensions[0] + pos.x]
+        x = pos.x
+        y = pos.y
+        if self.looping:
+            x = x % self.dimensions[0]
+            y = y % self.dimensions[1]
+        return self.original_tiles[y * self.dimensions[0] + x]
 
     def get_visual_tile(self, pos: Position):
-        return self.visual_tiles[pos.y * self.dimensions[0] + pos.x]
+        x = pos.x
+        y = pos.y
+        if self.looping:
+            x = x % self.dimensions[0]
+            y = y % self.dimensions[1]
+        return self.visual_tiles[y * self.dimensions[0] + x]
 
     def get_tile_string(self, pos: Position):
-        tile = self.visual_tiles[pos.y * self.dimensions[0] + pos.x]
+        tile = self.get_visual_tile(pos)
         if isinstance(tile, set):
             tile = "e"
         return self.tileset.colors.get(tile, colorama.Style.RESET_ALL) + tile
 
     def set_tile(self, pos: Position, value: str | set[str]):
-        self.original_tiles[pos.y * self.dimensions[0] + pos.x] = value
-        self.visual_tiles[pos[1] * self.dimensions[0] + pos[0]] = value
+        x = pos.x
+        y = pos.y
+        if self.looping:
+            x = x % self.dimensions[0]
+            y = y % self.dimensions[1]
+        self.original_tiles[y * self.dimensions[0] + x] = value
+        self.visual_tiles[y * self.dimensions[0] + x] = value
 
     def set_visual_tile(self, pos, value: str):
-        self.visual_tiles[pos[1] * self.dimensions[0] + pos[0]] = value
+        x = pos.x
+        y = pos.y
+        if self.looping:
+            x = x % self.dimensions[0]
+            y = y % self.dimensions[1]
+        self.visual_tiles[y * self.dimensions[0] + x] = value
 
     def clear_visual_tile(self, pos):
-        self.visual_tiles[pos[1] * self.dimensions[0] + pos[0]] = self.original_tiles[pos[1] * self.dimensions[0] + pos[0]]
+        x = pos.x
+        y = pos.y
+        if self.looping:
+            x = x % self.dimensions[0]
+            y = y % self.dimensions[1]
+        self.visual_tiles[y * self.dimensions[0] + x] = self.original_tiles[y * self.dimensions[0] + x]
 
     def get_valid_directions(self, pos: Position):
         return_directions = set()
+        if self.looping:
+            return Direction.all_cardinal()
         for direction in Direction.all_cardinal():
             new_pos = pos + direction.get_tuple()
             if not (new_pos.x < 0
@@ -42,6 +70,24 @@ class Map:
                     or new_pos.y >= self.dimensions[1]):
                 return_directions.add(direction)
         return return_directions
+
+    def tile_to_dimensions(self, new_dimensions: tuple[int, int]):
+        """
+            Resizes map to a new size.
+            If the new dimensions are large, tesselate.
+            Note: due to implementation, this clears all visual tiles
+        """
+        was_looping = self.looping
+        self.looping = True
+        new_tiles = []
+        for i in range(new_dimensions[0]):
+            for j in range(new_dimensions[1]):
+                target = Position(i, j)
+                new_tiles.append(self.get_tile(target))
+        self.original_tiles = new_tiles
+        self.visual_tiles = new_tiles
+        self.dimensions = new_dimensions
+        self.looping = was_looping
 
     # sets tiles in grid
     def __str__(self):
--- wavefunction_collapse.py
+++ wavefunction_collapse.py
@@ -1,24 +1,143 @@
+from os import truncate
 import random
 from position import Position, Direction
 from map import Map
+from tileset import Tileset
+import queue
+import itertools
+import threading
+import multiprocessing
 
 
-def map_generation(map: Map):
-    dimensions = map.dimensions
+def map_generation_chunked(tileset: Tileset, dimensions: tuple[int, int], chunk_dimensions: tuple[int, int], regen_entropy: bool = False, num_threads: int = 1, looping = False):
+    chunk = Map(chunk_dimensions, tileset, looping=True)
+    if chunk_dimensions[0] >= dimensions[0] and chunk_dimensions[1] >= dimensions[1]:
+        chunk.looping = looping
+        chunk.tile_to_dimensions(dimensions)
+        return map_generation(chunk, regen_entropy=regen_entropy)
+    map_generation(chunk)
+    chunk.looping = looping
+    chunk.tile_to_dimensions(dimensions)
+    return_map = chunk
+    
+    regen_offset = (chunk_dimensions[0] // 2, chunk_dimensions[1] // 2)
+    jobs = multiprocessing.Queue()
+    x_pos_list = [(x, chunk_dimensions[0]) for x in range(regen_offset[0], dimensions[0] + regen_offset[0], chunk_dimensions[0])]
+    y_pos_list = [(y, chunk_dimensions[1]) for y in range(regen_offset[1], dimensions[1] + regen_offset[1], chunk_dimensions[1])]
+    x_pos_list = map(lambda x: job_trimmer(x, dimensions[0], regen_offset[0], looping), x_pos_list)
+    y_pos_list = map(lambda y: job_trimmer(y, dimensions[1], regen_offset[1], looping), y_pos_list)
+    if not looping:
+        x_pos_list = itertools.chain([(0, regen_offset[0])], x_pos_list)
+        y_pos_list = itertools.chain([(0, regen_offset[1])], y_pos_list)
+
+    jobs_list = itertools.product(x_pos_list, y_pos_list)
+    jobs_list = list(map(job_zipper, jobs_list))
+
+    # This is required to ensure threads are less likely to work
+    # on neighboring chunks, which could lead to conflicts.
+    # I wish there was an easier way to checkerboard it like this.
+    jobs_list_even = []
+    jobs_list_odd = []
+    old_y = -1
+    last_even = False
+    last_row_even = False
+    
+    for job in jobs_list:
+        if job[0].y != old_y:
+            old_y = job[0].y
+            last_even = last_row_even
+            last_row_even = not last_row_even
+        if last_even:
+            jobs_list_odd.append(job)
+        else:
+            jobs_list_even.append(job)
+    for job in jobs_list_even:
+        jobs.put(job)
+    for job in jobs_list_odd:
+        jobs.put(job)
+    jobs.close()
+
+    processes = []
+    for i in range(num_threads):
+        processes.append(multiprocessing.Process(target=lambda: chunk_worker(return_map, jobs), name=f"Thread-{i}"))
+    for process in processes:
+        process.start()
+    for process in processes:
+        process.join()
+    jobs.join_thread()
+    return return_map
+
+def job_trimmer(job, dimension, offset, looping):
+    pos = job[0]
+    length = job[1]
+    return (pos, min(length, dimension + (offset if looping else 0) - pos))
+
+def job_zipper(job):
+    return (Position(job[0][0], job[1][0]), (job[0][1], job[1][1]))
+
+def chunk_worker(map, jobs):
+    label = multiprocessing.current_process().name
+    print(f"Starting thread {label}")
+    while True:
+        try:
+            job = jobs.get(timeout=0.2)
+        except (queue.Empty, OSError):
+            break
+        # print(f"Thread {label} working job {job}")
+        remove_section_and_repropagate(map, job[0], job[1])
+        map_generation(map, job, True)
+        # print(f"Thread {label} finished job {job}")
+    print(f"No more jobs in queue. Killing thread {label}")
+
+
+def remove_section_and_repropagate(map: Map, pos: Position, dimensions: tuple[int, int]):
+    end_pos = pos + (dimensions[0] - 1, dimensions[1] - 1)
     for i in range(dimensions[0]):
         for j in range(dimensions[1]):
-            tile_options = map.get_tile(Position(i, j))
+            target = pos + (i, j)
+            map.set_tile(target, map.tileset.tiles)
+    if pos.y > 0 or map.looping:
+        for i in range(dimensions[0]):
+            target = pos + (i, -1)
+            propagate_collapse(map, target, limit_directions={Direction.S})
+    if end_pos.y < map.dimensions[1] - 1 or map.looping:
+        for i in range(dimensions[0]):
+            target = end_pos + (-i, 1)
+            propagate_collapse(map, target, limit_directions={Direction.N})
+    if pos.x > 0 or map.looping:
+        for i in range(dimensions[1]):
+            target = pos + (-1, i)
+            propagate_collapse(map, target, limit_directions={Direction.E})
+    if end_pos.x < map.dimensions[0] - 1 or map.looping:
+        for i in range(dimensions[1]):
+            target = end_pos + (1, -i)
+            propagate_collapse(map, target, limit_directions={Direction.W})
+
+
+def map_generation(map: Map, limits: tuple[Position, tuple[int, int]] | None = None, regen_entropy: bool = False):
+    if limits is not None:
+        offset = limits[0]
+        dimensions = limits[1]
+    else:
+        offset = Position(0, 0)
+        dimensions = map.dimensions
+    for i in range(dimensions[0]):
+        for j in range(dimensions[1]):
+            target = Position(i, j) + offset
+            tile_options = map.get_tile(target)
             if isinstance(tile_options, set):
                 choice = random.choice(list(tile_options))
-                map.set_tile(Position(i, j), choice)
-                propagate_collapse(map, Position(i, j), None)
+                map.set_tile(target, choice)
+                propagate_collapse(map, target, None)
     return map
 
 
-def propagate_collapse(map: Map, position: Position, direction: Direction | None = None):
+def propagate_collapse(map: Map, position: Position, direction: Direction | None = None, limit_directions: set[Direction] | None = None):
     prop_source = map.get_tile(position)
     iter_directions = map.get_valid_directions(position)
     iter_directions -= {direction}
+    if limit_directions is not None:
+        iter_directions = iter_directions.intersection(limit_directions)
     for dir in iter_directions:
         target = position.traverse(dir)
         target_options = map.get_tile(target)
